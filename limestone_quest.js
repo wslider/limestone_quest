77218e04e3369a0a9418ce42e94255aabe01d02d
wslider <wslider2000@gmail.com>
2025-07-18 19:09:12 -0400

    characters and locations in js file

--- limestone_quest.js
+++ limestone_quest.js
@@ -1,4 +1,34 @@
+// global variables
+
 let mushroomCoffee = 10 
 let elevation = 5000
 let health = 100
 let mushroomCoffeeText = mushroomCoffee
+
+// characters
+
+const jenny = {
+id:jenny,
+name:jenny,
+class:character,
+health:100,
+inventory:["bone", "stick"],
+
+};
+
+const little = {
+id:little,
+name:jenny,
+class:character,
+health:100,
+inventory:["carrot", "herbs"],
+};
+
+//locations
+
+const limestoneCanyon = {
+id:limestoneCanyon,
+name:limestoneCanyon,
+class:location,
+hazards ["goblins", "falling rocks", "weather"], 
+}
